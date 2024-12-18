274731f0603894870280bec9f6b738cd4159794a
Sujan Kumar K <sujan.22cs164@sode-edu.in>
2024-12-18 21:00:52 +0530

    chatbot

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -4,15 +4,15 @@ const axiosInstance = axios.create({
     baseURL: 'https://mental-health-chatbot-server.vercel.app/api', 
 });
 
-axiosInstance.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response && error.response.status === 401) {
-            alert('Session expired. Please log in again.');
-            window.location.href = '/';
-        }
-        return Promise.reject(error);
-    }
-);
+// axiosInstance.interceptors.response.use(
+//     (response) => response,
+//     (error) => {
+//         if (error.response && error.response.status === 401) {
+//             alert('Session expired. Please log in again.');
+//             window.location.href = '/';
+//         }
+//         return Promise.reject(error);
+//     }
+// );
 
 export default axiosInstance;
